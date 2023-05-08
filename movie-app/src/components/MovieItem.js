716e8788580ecc40e87f6379edc49d5be6314e25
galeindor <73552373+galeindor@users.noreply.github.com>
2023-05-09 00:32:18 +0300

    added working read more button

--- movie-app/src/components/MovieItem.js
+++ movie-app/src/components/MovieItem.js
@@ -1,7 +1,10 @@
-import {Card} from "react-bootstrap";
-import {useState} from "react";
+import {Button, Card} from "react-bootstrap";
+import {useEffect, useState} from "react";
 export default function MovieItem({movie}) {
     const [isHovered, setIsHovered] = useState(false);
+    const [longOverview, setLongOverview] = useState(false);
+    const [overview, setOverview] = useState(movie.overview);
+    const [show, setShow] = useState(false);
     function MouseOver() {
         setIsHovered(true);
     }
@@ -9,11 +12,37 @@ export default function MovieItem({movie}) {
         setIsHovered(false);
     }
     const img_url = movie.poster_path ?`https://image.tmdb.org/t/p/original${movie.poster_path}` : './default.jpg';
+
+    useEffect(() => {
+        if(overview.length > 100) {
+            showLessOverview();
+        }
+    }, []);
+
+    function showLessOverview() {
+        console.log("show less");
+        const words = movie.overview.split(" ");
+        let newOverview = "";
+        while (newOverview.length < 100) {
+            newOverview += " " + words.shift();
+        }
+        newOverview += "...";
+        setOverview(newOverview);
+        if(!longOverview) setLongOverview(true);
+        setShow(false);
+    }
+
+    function showAllOverview() {
+        setOverview(movie.overview);
+        setShow(true);
+    }
+
     return (
         <Card onMouseEnter={MouseOver} onMouseLeave={MouseOut} style={{backgroundColor: isHovered ?"#CCD1D1" : "#E5E8E8",borderWidth: 3 , fontFamily: "serif"}}>
             <h2>{movie.title}</h2>
             <img src={img_url} alt={movie.title} className="img-fluid m-1"/>
-            <p className={"m-1"}>{movie.overview}</p>
+            <p className={"m-1"}>{overview}</p>
+            {longOverview && <Button variant="outline-secondary" className={"m-1"} onClick={show ? showLessOverview : showAllOverview } >Read more</Button>}
         </Card>
     )
 }
