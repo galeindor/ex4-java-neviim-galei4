858b52742181af3a08db3a736aca4371f880d0de
galeindor <73552373+galeindor@users.noreply.github.com>
2023-05-10 18:17:41 +0300

    basic empty cart handle

--- movie-app/src/components/ShoppingCartTable.js
+++ movie-app/src/components/ShoppingCartTable.js
@@ -6,30 +6,43 @@ import axios, {get} from "axios";
 
 export default function ShoppingCartTable() {
     const [cart, setCart] = useState([]);
+    const [isEmpty, setIsEmpty] = useState(true);
     useEffect(() => {
         const fetchCart = async () => {
             const response = await axios.get(REST_API_URL);
-           setCart(await response.data);
+            setCart(await response.data);
         }
         fetchCart().catch((e) => {
             console.log(e);
         });
     }, []);
-    return (
-        <Table striped bordered hover style={{fontFamily: "cursive"}}>
-            <thead>
-                <tr>
-                    <th>Movie Poster</th>
-                    <th>Movie title</th>
-                    <th>Price</th>
-                </tr>
-            </thead>
-            <tbody>
-                {cart.map((item) => (
-                    <CartItem key={item.id} item={item}/>
 
-                ))}
-            </tbody>
-        </Table>
+    useEffect(() => {
+        setIsEmpty(cart.length === 0);
+    }, [cart]);
+
+    return (
+        <>
+            {isEmpty &&
+                <h2 className="text-center mt-2 text-danger">Your cart is empty, Go Shop!</h2>
+            }
+            {!isEmpty &&
+                <Table striped bordered hover style={{fontFamily: "cursive"}}>
+                    <thead>
+                    <tr>
+                        <th>Movie Poster</th>
+                        <th>Movie title</th>
+                        <th>Price</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {cart.map((item) => (
+                        <CartItem key={item.id} item={item}/>
+
+                    ))}
+                    </tbody>
+                </Table>
+            }
+        </>
     )
 }
