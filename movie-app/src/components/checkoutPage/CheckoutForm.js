33ef65d90cf40e20aa1892c5abd588472506dda7
galeindor <73552373+galeindor@users.noreply.github.com>
2023-05-24 09:09:42 +0300

    created functionality for useFetch in CheckoutForm.js

--- movie-app/src/components/checkoutPage/CheckoutForm.js
+++ movie-app/src/components/checkoutPage/CheckoutForm.js
@@ -1,14 +1,14 @@
 import {Button, FloatingLabel, Form} from "react-bootstrap";
 import {CURRENCY, REST_API_CHECKOUT_URL} from "../../constants";
-import axios from "axios";
 import {useFetch} from "../../customHooks/useFetch";
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import LoadingSpinner from "../LoadingSpinner";
 
-export default function CheckoutForm({total, emptyCart}){
-    const [errors, setErrors] = useState({email: '', firstName: '', lastName: '', error: ''});
-    const [isLoading, setIsLoading] = useState(false);
+export default function CheckoutForm({total, emptyCart}) {
+    const [{data, isLoading, errors}, doFetch] = useFetch(false);
+    const [fieldErrors, setFieldErrors] = useState({email: '', firstName: '', lastName: '', error: ''});
+
     async function onSubmit(e) {
-        try {
             e.preventDefault();
             const data = {
                 email: e.target.email.value,
@@ -17,35 +17,31 @@ export default function CheckoutForm({total, emptyCart}){
                 payment: total
             }
             console.log(data);
-            if(!validateForm(data)) {
+            if (!validateForm(data)) {
                 return;
             }
-            setIsLoading(true);
-            const response = await axios.post(REST_API_CHECKOUT_URL, data, {
+            doFetch(REST_API_CHECKOUT_URL, "post", data);
+            /*const response = await axios.post(REST_API_CHECKOUT_URL, data, {
                 headers: {
                     'Content-Type': 'application/json'
                 },
                 body: JSON.stringify(data)
-            })
-            if (response.status === 200) {
-                await emptyCart();
-            }
-            if (response.status === 200) {
-                window.location.href = "/";
-            }
-
-        } catch (e) {
-            console.log(e);
-            if (e.response.status === 400) { // Bad Request
-                setErrors(e.response.data);
-            }
-            else {
-                setErrors({...errors , error: "Error checking out , please try again later"});
-            }
-        } finally {
-            setIsLoading(false);
-        }
+            })*/
     }
+
+    useEffect(() => {
+        if (errors && errors.status) {
+            if (errors.status === 400) {
+                setFieldErrors(errors);
+            } else {
+                setFieldErrors({...fieldErrors, error: "Error checking out , please try again later"});
+            }
+        }
+        if (data) {
+            emptyCart();
+            window.location.href = "/";
+        }
+    }, [data]);
 
     function validateEmail(email) {
         const re = /\S+@\S+\.\S+/;
@@ -58,44 +54,46 @@ export default function CheckoutForm({total, emptyCart}){
 
     function validateForm(data) {
         let errorList = {};
-        if(!validateEmail(data.email)) {
+        if (!validateEmail(data.email)) {
             errorList["email"] = "Invalid email";
         }
-        if(!validateName(data.firstName)) {
+        if (!validateName(data.firstName)) {
             errorList["firstName"] = "Invalid first name";
         }
-        if(!validateName(data.lastName)) {
+        if (!validateName(data.lastName)) {
             errorList["lastName"] = "Invalid last name";
         }
-        setErrors(errorList);
+        setFieldErrors(errorList);
         return Object.keys(errorList).length === 0;
     }
 
-    return(
-        <Form className={"mt-3"} onSubmit={onSubmit}>
-            {errors.error && <Form.Text className="text-danger">{errors.error}</Form.Text>}
-            <FloatingLabel label="Email address" className="mb-3"
-            >
-                <Form.Control name="email" type="email" placeholder="Email"/>
-                {errors.email && <Form.Text className="text-danger">{errors.email}</Form.Text>}
-            </FloatingLabel>
+    return (
+        <>
+            <Form className={"mt-3"} onSubmit={onSubmit}>
+                {fieldErrors.error && <Form.Text className="text-danger">{fieldErrors.error}</Form.Text>}
+                <FloatingLabel label="Email address" className="mb-3"
+                >
+                    <Form.Control name="email" type="email" placeholder="Email"/>
+                    {fieldErrors.email && <Form.Text className="text-danger">{fieldErrors.email}</Form.Text>}
+                </FloatingLabel>
 
-            <FloatingLabel label="First Name" className="mb-3">
-                <Form.Control name="firstName" type="text" placeholder="First Name"/>
-                {errors.firstName && <Form.Text className="text-danger">{errors.firstName}</Form.Text>}
-            </FloatingLabel>
+                <FloatingLabel label="First Name" className="mb-3">
+                    <Form.Control name="firstName" type="text" placeholder="First Name"/>
+                    {fieldErrors.firstName && <Form.Text className="text-danger">{fieldErrors.firstName}</Form.Text>}
+                </FloatingLabel>
 
-            <FloatingLabel label="Last Name" className="mb-3">
-                <Form.Control name="lastName" type="text" placeholder="Last Name"/>
-                {errors.lastName && <Form.Text className="text-danger">{errors.lastName}</Form.Text>}
-            </FloatingLabel>
+                <FloatingLabel label="Last Name" className="mb-3">
+                    <Form.Control name="lastName" type="text" placeholder="Last Name"/>
+                    {fieldErrors.lastName && <Form.Text className="text-danger">{fieldErrors.lastName}</Form.Text>}
+                </FloatingLabel>
 
-            <FloatingLabel label="Total" className="mb-3">
-                <Form.Control type="text" value={total + CURRENCY} name={"payment"} disabled/>
-            </FloatingLabel>
+                <FloatingLabel label="Total" className="mb-3">
+                    <Form.Control type="text" value={total + CURRENCY} name={"payment"} disabled/>
+                </FloatingLabel>
 
-            <Button variant="primary" type="submit">Submit</Button>
-
-        </Form>
+                <Button variant="primary" type="submit">Submit</Button>
+            </Form>
+            {isLoading && <LoadingSpinner/>}
+        </>
     )
 }
